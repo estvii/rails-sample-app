ddc979391b0d7802edfc81a2911a227a837b4313
Steven Vi <steven@192-168-1-11.tpgi.com.au>
2018-10-12 14:37:48 +1100

    Implement Basic Login

--- test/helpers/test_helper.rb
+++ test/helpers/test_helper.rb
@@ -9,5 +9,11 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
+  # Returns true if a test user is logged in not working with with the users_signup_test
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
+
 end
